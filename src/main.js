86a43713b43a6b3d8f972c341c416ddbff390f9d
DrDxterLA <cristobal.riffo.berna@gmail.com>
2026-03-14 00:37:01 -0300

    Integración de Bootstrap y estilos globales

--- src/main.js
+++ src/main.js
@@ -2,8 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-const app = createApp(App)
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+import 'aos/dist/aos.css'
 
-app.use(router)
-
-app.mount('#app')
+createApp(App)
+.use(router)
+.mount('#app')

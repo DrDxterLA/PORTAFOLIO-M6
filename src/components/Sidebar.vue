3791337a70a8780521c9549b0bea154f3db191db
DrDxterLA <cristobal.riffo.berna@gmail.com>
2026-03-14 01:09:30 -0300

    Corrección del menú Sidebar y aplicación de estilos globales para visualización correcta

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -5,23 +5,22 @@
 <div class="text-center">
 
 <h3>Cristobal Riffo</h3>
-
 <p>Desarrollador & Productor</p>
 
 </div>
 
 <ul class="nav flex-column mt-4">
 
-<li>
-<router-link to="/">Inicio</router-link>
+<li class="nav-item">
+<router-link class="nav-link" to="/">Inicio</router-link>
 </li>
 
-<li>
-<router-link to="/about">Sobre mí</router-link>
+<li class="nav-item">
+<router-link class="nav-link" to="/about">Sobre mí</router-link>
 </li>
 
-<li>
-<router-link to="/contact">Contacto</router-link>
+<li class="nav-item">
+<router-link class="nav-link" to="/contact">Contacto</router-link>
 </li>
 
 </ul>

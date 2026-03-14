2a967962c36741da2edda07fffb46d6330942f35
DrDxterLA <cristobal.riffo.berna@gmail.com>
2026-03-14 00:38:13 -0300

    Creación del componente Hero con datos reactivos

---
+++ src/components/Hero.vue
@@ -0,0 +1,19 @@
+<template>
+
+<section class="hero text-center">
+
+<h1>{{ name }}</h1>
+
+<p>{{ description }}</p>
+
+</section>
+
+</template>
+
+<script setup>
+
+const name = "Cristobal Riffo"
+
+const description = "Desarrollador Web | Fotógrafo | Productor de Eventos"
+
+</script>

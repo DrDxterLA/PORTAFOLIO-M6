dc11ea1f297cc5b3b275229b85158dd0c2c46c1d
DrDxterLA <cristobal.riffo.berna@gmail.com>
2026-03-14 00:37:42 -0300

    Creación del componente Sidebar con navegación

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,31 @@
+<template>
+
+<nav id="sidebar">
+
+<div class="text-center">
+
+<h3>Cristobal Riffo</h3>
+
+<p>Desarrollador & Productor</p>
+
+</div>
+
+<ul class="nav flex-column mt-4">
+
+<li>
+<router-link to="/">Inicio</router-link>
+</li>
+
+<li>
+<router-link to="/about">Sobre mí</router-link>
+</li>
+
+<li>
+<router-link to="/contact">Contacto</router-link>
+</li>
+
+</ul>
+
+</nav>
+
+</template>

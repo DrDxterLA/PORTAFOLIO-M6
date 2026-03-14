d4f5ac9c228e6cff6b8af134cc98ddee90561d56
DrDxterLA <cristobal.riffo.berna@gmail.com>
2026-03-14 00:40:18 -0300

    Implementación del portafolio dinámico

---
+++ src/components/Portfolio.vue
@@ -0,0 +1,41 @@
+<template>
+
+<section class="container">
+
+<h2>Portafolio</h2>
+
+<div class="row">
+
+<div
+v-for="project in projects"
+:key="project.title"
+class="col-lg-4 mb-4"
+>
+
+<div class="card">
+
+<img :src="project.image">
+
+<div class="card-body">
+
+<h5>{{project.title}}</h5>
+
+<p>{{project.description}}</p>
+
+</div>
+
+</div>
+
+</div>
+
+</div>
+
+</section>
+
+</template>
+
+<script setup>
+
+import projects from '../data/portfolio'
+
+</script>
---
+++ src/data/portfolio.js
@@ -0,0 +1,21 @@
+export default [
+
+{
+title:"App Clima Vue",
+description:"Aplicación que consume API del clima",
+image:"https://picsum.photos/400/300"
+},
+
+{
+title:"Landing Page Vue",
+description:"Landing page usando props",
+image:"https://picsum.photos/401/300"
+},
+
+{
+title:"Sistema Fotografías",
+description:"Sistema web para entrega de fotos",
+image:"https://picsum.photos/402/300"
+}
+
+]
